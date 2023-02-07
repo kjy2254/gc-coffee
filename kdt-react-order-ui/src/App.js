66c7f88cdfb4759ddce9de5141621c67ef171bd0
JunyeongKim <wkrwjafl15@gmail.com>
2023-02-07 18:11:44 +0900

    클론코딩

--- kdt-react-order-ui/src/App.js
+++ kdt-react-order-ui/src/App.js
@@ -1,95 +1,52 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import React, {useState} from 'react';
-
-function Product(props) {
-    const productName = props.productName;
-    const category = props.category;
-    const price = props.price;
-    return (
-        <>
-            <div className="col-2"><img className="img-fluid"
-                                        src="https://i.imgur.com/HKOFQYa.jpeg"
-                                        alt=""/></div>
-            <div className="col">
-                <div className="row text-muted">{productName}</div>
-                <div className="row">{category}</div>
-            </div>
-            <div className="col text-center price">{price}</div>
-            <div className="col text-end action">
-                <button className="btn btn-small btn-outline-dark">추가</button>
-            </div>
-        </>
-    )
-}
-
-function ProductList(props) {
-    const products = props.products;
-    return (
-        <React.Fragment>
-            <div className="col-md-8 mt-4 d-flex flex-column align-items-start p-3 pt-0">
-                <h5 className="flex-grow-0"><b>상품 목록</b></h5>
-                <ul className="list-group products">
-                    {products.map(v => {
-                        <li key={v.id} className="list-group-item d-flex mt-3">
-                            <Product productName={v.productName} category={v.category} price={v.price}/>
-                        </li>
-                    })}
-                </ul>
-            </div>
-        </React.Fragment>
-    )
-}
-
-function Summary() {
-    return (
-        <React.Fragment>
-            <div className="col-md-4 summary p-4">
-                <div>
-                    <h5 className="m-0 p-0"><b>Summary</b></h5>
-                </div>
-                <hr/>
-                <div className="row">
-                    <h6 className="p-0">Columbia Nariñó <span className="badge bg-dark text-">2개</span></h6>
-                </div>
-                <div className="row">
-                    <h6 className="p-0">Brazil Serra Do Caparaó <span className="badge bg-dark">2개</span>
-                    </h6>
-                </div>
-                <div className="row">
-                    <h6 className="p-0">Columbia Nariñó <span className="badge bg-dark">2개</span></h6>
-                </div>
-                <form>
-                    <div className="mb-3">
-                        <label htmlFor="email" className="form-label">이메일</label>
-                        <input type="email" className="form-control mb-1" id="email"/>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="address" className="form-label">주소</label>
-                        <input type="text" className="form-control mb-1" id="address"/>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="postcode" className="form-label">우편번호</label>
-                        <input type="text" className="form-control" id="postcode"/>
-                    </div>
-                    <div>당일 오후 2시 이후의 주문은 다음날 배송을 시작합니다.</div>
-                </form>
-                <div className="row pt-2 pb-2 border-top">
-                    <h5 className="col">총금액</h5>
-                    <h5 className="col text-end">15000원</h5>
-                </div>
-                <button className="btn btn-dark col-12">결제하기</button>
-            </div>
-        </React.Fragment>
-    )
-}
+import React, {useEffect, useState} from 'react';
+import {ProductList} from "./components/ProductList";
+import {Summary} from "./components/Summary";
+import axios from "axios";
 
 function App() {
-    const [products, setProducts] = useState([
-        {id: 'uuid-1', productName: '콜롬비아 커피1', category: '커피빈1', price: 3000},
-        {id: 'uuid-1', productName: '콜롬비아 커피2', category: '커피빈2', price: 3000},
-        {id: 'uuid-1', productName: '콜롬비아 커피3', category: '커피빈3', price: 3000}
-    ]);
+    const [products, setProducts] = useState([]);
+    const [items, setItems] = useState([]);
+    const handleAddClicked = id => {
+        const product = products.find(v => v.id === id);
+        const found = items.find(v => v.id === id);
+        const updatedItems = found ?
+            items.map(v => (v.id === id) ? {...v, count: v.count + 1} : v)
+            : [...items, {...product, count: 1}];
+        setItems(updatedItems);
+    }
+
+    useEffect(() => {
+        axios.get('/api/v1/products')
+            .then(v => {
+                setProducts(v.data);
+                console.log(products);
+            })
+    }, [])
+
+    function handleOrderSubmit(order) {
+        if (items.length === 0) {
+            alert("아이템을 추가해 주세요!");
+        } else {
+            axios.post('/api/v1/orders', {
+                email: order.email,
+                address: order.address,
+                postcode: order.postcode,
+                orderItems: items.map(v => ({
+                    productId: v.productId,
+                    category: v.category,
+                    price: v.price,
+                    quantity: v.quantity
+                }))
+            }).then(
+                v => alert("주문이 정상적으로 접수되었습니다."),
+                e => {
+                    alert("서버 장애");
+                    console.error(e);
+                })
+        }
+    }
 
     return (<div className="App">
         <div className="container-fluid">
@@ -98,8 +55,8 @@ function App() {
             </div>
             <div className="card">
                 <div className="row">
-                    <ProductList products={products}/>
-                    <Summary/>
+                    <ProductList products={products} onAddClick={handleAddClicked}/>
+                    <Summary items={items} onOrderSubmit={handleOrderSubmit}/>
                 </div>
             </div>
         </div>
---
+++ src/main/java/com/example/gccoffee/controller/api/OrderRestController.java
@@ -0,0 +1,31 @@
+package com.example.gccoffee.controller.api;
+
+import com.example.gccoffee.controller.CreateOrderRequest;
+import com.example.gccoffee.model.Category;
+import com.example.gccoffee.model.Email;
+import com.example.gccoffee.model.Order;
+import com.example.gccoffee.model.Product;
+import com.example.gccoffee.service.OrderService;
+import com.example.gccoffee.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class OrderRestController {
+    @Autowired
+    private final OrderService orderService;
+
+    @GetMapping("/api/v1/orders")
+    public Order createOrder(@RequestBody CreateOrderRequest orderRequest) {
+        return orderService.createOrder(
+                new Email(orderRequest.email()),
+                orderRequest.address(),
+                orderRequest.postcode(),
+                orderRequest.orderItems()
+        );
+    }
+}
